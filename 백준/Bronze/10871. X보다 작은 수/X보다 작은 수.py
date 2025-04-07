0b4d7772dc6670caac2a72429c6201ff927827cd
Seongwoo Jang <166791301+woo1016@users.noreply.github.com>
2025-04-07 21:24:07 +0900

    [Bronze V] Title: X보다 작은 수, Time: 40 ms, Memory: 33432 KB -BaekjoonHub

---
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.py
@@ -0,0 +1,10 @@
+n, x = map(int, input().split())
+a = list(map(int, input().split()))
+
+b = []
+for i in range(n):
+    if a[i] < x:
+     b.append(a[i])
+
+for i in range(len(b)):
+    print(b[i], end=' ')
